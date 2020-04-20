951d54983cbd7dd8f8f30c4a28bfd88b0545a91a
unknown <jp4dr@virginia.edu>
2020-04-20 14:02:29 -0400

    so close

--- log_in.php
+++ log_in.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-$db_connection = pg_connect("ec2-52-71-231-180.compute-1.amazonaws.com
-port=5432 dbname=d5g84sufgsvitc user=elybocxzzsvtjp password=7611f8ff31e698d42a0571c4a27b74f3bc83034ce58321bd5291711b7516aca7
+$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
+ port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
 $username = $_POST['username'];
 $password = $_POST['password'];
--- signup.php
+++ signup.php
@@ -5,27 +5,27 @@ $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
 $username = $_POST['username'];
-$email = $_POST['email'];
-$address = $_POST['address'];
+//$email = $_POST['email'];
+//$address = $_POST['address'];
 $password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-$city = $_POST['city'];
-$zip = $_POST['zip'];
-$state = $_POST['state'];
+//$city = $_POST['city'];
+//$zip = $_POST['zip'];
+//$state = $_POST['state'];
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 
-$query = "SELECT* FROM site_users WHERE username='$username' OR email='$email'";
+$query = "SELECT* FROM site_users WHERE username='$username'";
 $result = pg_query($db_connection,$query);
 $user = pg_fetch_assoc($result);
 
 
 if(!$user) {
-    $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$email','$address','$city','$state','$zip','$username','$hashed_password')";
+    $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$username','$hashed_password')";
     $result_1 = pg_query($db_connection,$query_1);
     
     $_SESSION['username'] = $username;
-    $_SESSION['email'] = $email;
+    //$_SESSION['email'] = $email;
     $_SESSION['logged_in'] = "logged_in";
     
     //change to homepage for members
