380b903a09a07e8ccc556b5430005cc0e222bd94
unknown <jp4dr@virginia.edu>
2020-04-20 12:00:10 -0400

    connection check

--- signup.php
+++ signup.php
@@ -4,6 +4,10 @@ session_start();
 $db_connection = pg_connect("ec2-52-71-231-180.compute-1.amazonaws.com
  port=5432 dbname=d5g84sufgsvitc user=elybocxzzsvtjp password=7611f8ff31e698d42a0571c4a27b74f3bc83034ce58321bd5291711b7516aca7
 ");
+if(!$db_connection){
+	echo "<p>Could not connect to the server '" . $hostname . "'</p>\n";
+	header('Location: index.html');
+}
 $username = $_POST['username'];
 //$email = $_POST['email'];
 //$address = $_POST['address'];
