256c77e285656805e97e9c99bc86754c0cb522f0
unknown <jp4dr@virginia.edu>
2020-04-20 13:52:23 -0400

    copying from previous code

--- signup.php
+++ signup.php
@@ -1,51 +1,38 @@
 <?php
 session_start();
 
-$db_connection = pg_connect("ec2-52-71-231-180.compute-1.amazonaws.com
- port=5432 dbname=d5g84sufgsvitc user=elybocxzzsvtjp password=7611f8ff31e698d42a0571c4a27b74f3bc83034ce58321bd5291711b7516aca7
+$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
+ port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
-if(!$db_connection){
-	echo "<p>Could not connect to the server '" . $hostname . "'</p>\n";
-	header('Location: login.html');//need to change this later
-}
-//$username = $_POST['username'];
-$username = "jp4dr";
-$password = "@Jbperr98";
-//$email = $_POST['email'];
-//$address = $_POST['address'];
-//$password = $_POST['password'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-//$city = $_POST['city'];
-//$zip = $_POST['zip'];
-//$state = $_POST['state'];
-//$first_name = $_POST['first_name'];
-//$last_name = $_POST['last_name'];
-$first_name = "James";
-$last_name = "Perry";
+$city = $_POST['city'];
+$zip = $_POST['zip'];
+$state = $_POST['state'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 
-$query = "SELECT* FROM site_users WHERE username='$username'";
+$query = "SELECT* FROM site_users WHERE username='$username' OR email='$email'";
 $result = pg_query($db_connection,$query);
 $user = pg_fetch_assoc($result);
 
 
-//if(!$user) {
-    $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$username','$hashed_password')";
-	$result_1 = pg_query($db_connection,$query_1);
-	if($result_1){
-		$_SESSION['username'] = $username;
-		// $_SESSION['email'] = $email;
-		$_SESSION['logged_in'] = "logged_in";
-		
-		//change to homepage for members
-		header('Location: calendar.html');
-	}
-	else{
-		header('Location: index.html');
-	}
+if(!$user) {
+    $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$email','$address','$city','$state','$zip','$username','$hashed_password')";
+    $result_1 = pg_query($db_connection,$query_1);
     
-//}else{
-  //  header('Location: index.html');
-//}
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    $_SESSION['logged_in'] = "logged_in";
+    
+    //change to homepage for members
+    header('Location: calendar.html');
+}else{
+    header('Location: index.html');
+}
 
 
 
