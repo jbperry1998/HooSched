12d789ddb3b3df7c75bfcd21c2c04789e76e4554
unknown <jp4dr@virginia.edu>
2020-04-22 11:21:14 -0400

    finishing php for settings and add

--- add_event.php
+++ add_event.php
@@ -10,18 +10,36 @@
 		$username = $_SESSION['username'];
 		// $name = $_SESSION['name'];
 
+		$event_ID = $_POST['event_ID'];
+		$name = $_POST['name'];
+		$start_time = $_POST['start_time'];
+		$end_time = $_POST['end_time'];
+		$date = $_POSt['date'];
+		$description = $_POST['description'];
+		$frequency = $_POST['frequency'];
+
+		//extracurricular
 		$school = $_POST['school'];
-		$org_ID = $_POST['org_ID'];
+		$location = $_POST['location'];
+
+		//comunity
+		$size = $_POST['size'];
+		$venue = $_POST['venue'];
+		$owner_ID = $_POST['owner_ID'];
+
+		
 
 		if(isset($school)){
 			//insert username and school into students table
-			$query_1 = "INSERT INTO Student VALUES('$username','$school')";
+			$query_1 = "INSERT INTO Extracurricular VALUES('$event_ID','$username','$school','$location')";
     		$result_1 = pg_query($db_connection,$query_1);
 		}
-		if(isset($org_ID)){
-			$query_2 = "INSERT INTO Admin VALUES('$org_ID','$username')";
+		if(isset($size)){
+			$query_2 = "INSERT INTO Community VALUES('$event_ID','$username','$owner_ID','$size','$venue')";
     		$result_2 = pg_query($db_connection,$query_2);
 		}
+		$query = "INSERT INTO Event VALUES('$event_ID','$username','$name','$date','$description','$frequency','$start_time','$end_time')";
+		$result = pq_query($db_connection, $query);
 		header('Location: calendar.html');
 
 ?>
