9927c71f59be97b4cba02159f53140e70fe82ead
unknown <jp4dr@virginia.edu>
2020-04-22 22:58:56 -0400

    fetch stuff

--- fetch-event.php
+++ fetch-event.php
@@ -1,16 +1,23 @@
 <?php
-    require_once "db.php";
+	session_start();
+	$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
+	port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
+	");
 
-    $json = array();
-    $sqlQuery = "SELECT * FROM tbl_events ORDER BY id";
+    require_once "db.php";//idk what this does?
 
-    $result = mysqli_query($conn, $sqlQuery);
+	$user_ID = $_SESSION['username'];
+	$json = array();
+	
+    $query = "SELECT * FROM events WHERE user_ID = '$user_ID'";
+	$result = pg_query($db_connection, $query);
+	
     $eventArray = array();
-    while ($row = mysqli_fetch_assoc($result)) {
+    while ($row = pg_fetch_assoc($result)) {
         array_push($eventArray, $row);
     }
-    mysqli_free_result($result);
+    //pg_free_result($result);
 
-    mysqli_close($conn);
+    //mysqli_close($conn);
     echo json_encode($eventArray);
 ?>
