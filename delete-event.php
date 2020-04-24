68ac3f776a4766aa8be23e6e177a581e822c2555
madi0899 <37746443+madi0899@users.noreply.github.com>
2020-04-23 20:10:10 -0400

    delete-event calendar update

--- delete-event.php
+++ delete-event.php
@@ -1,8 +1,8 @@
 <?php
 require_once "db.php";
 
-$id = $_POST['id'];
-$sqlDelete = "DELETE from tbl_events WHERE id=".$id;
+$title = isset($_POST['title']) ? $_POST['title'] : "";
+$sqlDelete = "DELETE from tbl_events WHERE title=".$title;
 
 mysqli_query($conn, $sqlDelete);
 echo mysqli_affected_rows($conn);
