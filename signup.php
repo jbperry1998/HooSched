b620f45b01708ec5409acc0ec4918481a7d73b15
unknown <jp4dr@virginia.edu>
2020-04-24 12:20:20 -0400

    pdo

--- signup.php
+++ signup.php
@@ -28,7 +28,7 @@ catch(PDOException $e)
 	{
 	echo $sql . "<br>" . $e->getMessage();
 	}
-	$conn = null;
+	//$conn = null;
 
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
@@ -47,15 +47,17 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 
-$query = "SELECT* FROM site_users WHERE username='$username'";
-$result = pg_query($db_connection,$query);
-$user = pg_fetch_assoc($result);
+//$query = "SELECT* FROM site_users WHERE username='$username'";
+//$result = pg_query($db_connection,$query);
+//$user = pg_fetch_assoc($result);
+//$conn->exec($query);
 
 
-if(!$user) {
+//if(!$user) {
 
     $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$username','$hashed_password','$username')";
-	$result_1 = pg_query($db_connection,$query_1);
+	//$result_1 = pg_query($db_connection,$query_1);
+	$conn->exec($query_1);
 	 $_SESSION['username'] = $username;
 		$_SESSION['user_table'] = $username;
 		//$_SESSION['email'] = $email;
@@ -63,10 +65,7 @@ if(!$user) {
 		header('Location: rescalendar.html');
    
     //change to homepage for members
-    header('Location: rescalendar.html');
-}else{
-    header('Location: index.html');
-}
+
 
 
 
