bc74e8e66a6278ac68968dc9b1ff51a9932aa670
unknown <jp4dr@virginia.edu>
2020-04-24 12:01:29 -0400

    pdo

--- signup.php
+++ signup.php
@@ -4,7 +4,30 @@ $servername = "ec2-174-129-227-80.compute-1.amazonaws.com";
 $name = "zdlwovjrekrdar";
 $password = "ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5";
 $dbname = "dbvs140f5cqkp1";
-
+try {
+	$conn = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $name, $password);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// sql to create table
+	$sql = "CREATE TABLE IF NOT EXISTS $name(
+		event_ID character varying(50) NOT NULL,
+		name character varying(50) NOT NULL,
+		date date NOT NULL,
+		description character varying(100) NOT NULL,
+		frequency character varying(20) NOT NULL,
+		start_time timestamp without time zone,
+		end_time timestamp without time zone
+		)";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+	//echo "Table employees created successfully";
+	
+	}
+catch(PDOException $e)
+	{
+	echo $sql . "<br>" . $e->getMessage();
+	header('Location: index.html');
+	}
 
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
@@ -31,35 +54,12 @@ $user = pg_fetch_assoc($result);
 if(!$user) {
     $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$username','$hashed_password','$username')";
 	$result_1 = pg_query($db_connection,$query_1);
-	try {
-		$conn = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $name, $password);
-		// set the PDO error mode to exception
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// sql to create table
-		$sql = "CREATE TABLE IF NOT EXISTS $username(
-			event_ID character varying(50) NOT NULL,
-			name character varying(50) NOT NULL,
-			date date NOT NULL,
-			description character varying(100) NOT NULL,
-			frequency character varying(20) NOT NULL,
-			start_time timestamp without time zone,
-			end_time timestamp without time zone
-			)";
-		// use exec() because no results are returned
-		$conn->exec($sql);
-		//echo "Table employees created successfully";
-		$_SESSION['username'] = $username;
+
+    $_SESSION['username'] = $username;
 		$_SESSION['user_table'] = $username;
 		//$_SESSION['email'] = $email;
 		$_SESSION['logged_in'] = "logged_in";
 		header('Location: rescalendar.html');
-		}
-	catch(PDOException $e)
-		{
-		echo $sql . "<br>" . $e->getMessage();
-		header('Location: index.html');
-		}
-    
     //change to homepage for members
     header('Location: rescalendar.html');
 }else{
