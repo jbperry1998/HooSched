255bcdf2b8e37713b4393cbbfa176e05d8037f15
unknown <jp4dr@virginia.edu>
2020-04-24 11:46:52 -0400

    fixing links

--- signup.php
+++ signup.php
@@ -1,34 +1,11 @@
 <?php
 session_start();
 $servername = "ec2-174-129-227-80.compute-1.amazonaws.com";
-$username = "zdlwovjrekrdar";
+$name = "zdlwovjrekrdar";
 $password = "ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5";
 $dbname = "dbvs140f5cqkp1";
-try {
-    $conn = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // sql to create table
-    $sql = "CREATE TABLE IF NOT EXISTS $username(
-		event_ID character varying(50) NOT NULL,
-		name character varying(50) NOT NULL,
-		date date NOT NULL,
-		description character varying(100) NOT NULL,
-		frequency character varying(20) NOT NULL,
-		start_time timestamp without time zone,
-		end_time timestamp without time zone
-		)";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-	echo "Table employees created successfully";
-	header('Location: rescalendar.html');
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-$conn = null;
-/*
+
+
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
@@ -54,20 +31,30 @@ $user = pg_fetch_assoc($result);
 if(!$user) {
     $query_1 = "INSERT INTO site_users VALUES('$first_name','$last_name','$username','$hashed_password','$username')";
 	$result_1 = pg_query($db_connection,$query_1);
-	$quert_2 = "CREATE TABLE IF NOT EXISTS $username(
-		event_ID character varying(50) NOT NULL,
-		name character varying(50) NOT NULL,
-		date date NOT NULL,
-		description character varying(100) NOT NULL,
-		frequency character varying(20) NOT NULL,
-		start_time timestamp without time zone,
-		end_time timestamp without time zone
-		)";
-	//$create_table = $db->exec($query_2);
-	$result_2 = pg_query($db_connection,$query_2);
-    if(!$result_2){
-		header('Location: index.html');
-	}
+	try {
+		$conn = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $name, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// sql to create table
+		$sql = "CREATE TABLE IF NOT EXISTS $username(
+			event_ID character varying(50) NOT NULL,
+			name character varying(50) NOT NULL,
+			date date NOT NULL,
+			description character varying(100) NOT NULL,
+			frequency character varying(20) NOT NULL,
+			start_time timestamp without time zone,
+			end_time timestamp without time zone
+			)";
+		// use exec() because no results are returned
+		$conn->exec($sql);
+		echo "Table employees created successfully";
+		header('Location: rescalendar.html');
+		}
+	catch(PDOException $e)
+		{
+		echo $sql . "<br>" . $e->getMessage();
+		}
+	$conn = null;
 	$_SESSION['username'] = $username;
 	$_SESSION['user_table'] = $username;
     //$_SESSION['email'] = $email;
@@ -79,6 +66,6 @@ if(!$user) {
     header('Location: index.html');
 }
 
-*/
+
 
 ?>
