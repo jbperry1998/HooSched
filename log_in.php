387dce8946c7b238534d1e477fddfe35e75f7d05
Megan Reddy <mr8vn@virginia.edu>
2020-04-26 17:50:07 -0400

    still fixing login

--- log_in.php
+++ log_in.php
@@ -4,7 +4,7 @@ session_start();
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
-$username = $_POST['email'];
+$username = $_POST['username'];
 $password = $_POST['password'];
 $query = "SELECT * FROM site_users WHERE username='$username'";
 $result = pg_query($db_connection, $query);
@@ -16,7 +16,7 @@ if (! $result) {
 $row = pg_fetch_row($result);
 $hp = $row[3];
 if (password_verify($password, $hp)) {
-    $_SESSION['email'] = $username;
+    $_SESSION['username'] = $username;
 	$_SESSION['logged_in'] = "logged_in";
 	$_SESSION['user_table'] = $username;
 
