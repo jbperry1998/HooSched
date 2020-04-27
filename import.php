968a1ab542dcb022753533380a9db0751a2e933f
Annie Chapman <amc6wf@virginia.edu>
2020-04-26 23:02:12 -0400

    ok

--- import.php
+++ import.php
@@ -119,7 +119,7 @@ $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
 			header('Location: elements.html');
 		}
 
-$target_dir = "uploads/"; 
+$target_dir = "/uploads/"; 
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
