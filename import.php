b703df3845c6584d018d85645eaf7c1504bf9f8d
Annie Chapman <amc6wf@virginia.edu>
2020-04-26 20:19:32 -0400

    Update import.php

--- import.php
+++ import.php
@@ -108,7 +108,7 @@ FormStuff.init();
 				<h2>Import</h2>
 			</header>
 
-            <?php
+<?php
 session_start();
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
 		port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
@@ -157,7 +157,7 @@ if ( isset($_POST["submit"]) ) {
                         } 
                         else{
 
-                                $handle = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+                                $handle = fopen($target_file, "r");
                                 while($data = fgetcsv($handle)){
                                     $class_ID = data[0];
                                     $className = data[1];
@@ -219,76 +219,3 @@ if ( isset($_POST["submit"]) ) {
 
 </body>
 </html>
-
-<?php
-session_start();
-$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
-		port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
-		");
-		$login_status = $_SESSION['logged_in'];
-		if (! strcmp($login_status, "logged_in") == 0) {
-			header('Location: elements.html');
-		}
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-if ( isset($_POST["submit"]) ) {
-
-    if ( isset($_FILES["fileToUpload"])) {
- 
-             //if there was an error uploading the file
-         if ($_FILES["fileToUpload"]["error"] > 0) {
-             echo "Return Code: " . $_FILES["fileToUpload"]["error"] . "<br />";
- 
-         }
-         else {
-                  //if file already exists
-              if (file_exists("upload/" . $_FILES["fileToUpload"]["name"])) {
-             echo $_FILES["file"]["name"] . " already exists. ";
-              }
-              else {
-                // Check file size
-                if ($_FILES["fileToUpload"]["size"] > 500000) {
-                    echo "Sorry, your file is too large.";
-                    $uploadOk = 0;
-                }
-                else{
-                    // Allow certain file formats
-                    if($fileType != "csv") {
-                        echo "Sorry, only csv files are allowed.";
-                        $uploadOk = 0;
-                    }
-                    else{
-                        // Check if $uploadOk is set to 0 by an error
-                        if ($uploadOk == 0) {
-                            echo "Sorry, your file was not uploaded.";
-                        // if everything is ok, try to upload file
-                        } 
-                        else{
-
-                                $handle = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-                                while($data = fgetcsv($handle)){
-                                    $class_ID = data[0];
-                                    $className = data[1];
-                                    $location = data[2];
-                                    $start_time = data[3];
-                                    $end_time = data[4];
-                                    $days = data[5];
-                                    $teacher = data[6];
-                                    $query = "INSERT INTO class(class_ID, className, location, start_time, end_time, days, teacher) VALUES('".$class_ID."', '".className."', '".$location."', '".$start_time."', '".$end_time."', '".$days."', '".$teacher."')";
-    	                            $result_1 = pg_query($db_connection,$query);
-                                }
-                                fclose($handle);
-                                print("Import Successful");
-                            
-                        } 
-                    }
-                }
-            }
-        }
-    } 
-}
-?>
